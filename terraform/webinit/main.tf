7d2cc2fc5022e34b3ff6355d3ddf0c21c93f80e3
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-06-14 04:47:12 +0200

    fix: update cache control for website files to improve content freshness

--- terraform/webinit/main.tf
+++ terraform/webinit/main.tf
@@ -42,7 +42,7 @@ resource "google_storage_bucket_object" "website_files" {
     regex("\\.[^.]+$", each.value),
     "application/octet-stream"
   )
-  cache_control = "public, max-age=60"
+  cache_control = "public, max-age=0, s-maxage=60"
 }
 
 resource "cloudflare_record" "website_dns" {
